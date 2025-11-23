153d7e7b30d0ab2ec9a71c4dac34920c00c6c3c1
snjava <scnaikwadi2@gmail.com>
2025-11-23 13:37:14 +0530

    Servlet Life Cycle
    Servlet Life Cycle

--- code/AdvanceJava/ServletCommunication/src/main/java/ProductInfo.java
+++ code/AdvanceJava/ServletCommunication/src/main/java/ProductInfo.java
@@ -13,13 +13,14 @@ public class ProductInfo extends HttpServlet {
     protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         resp.setContentType("text/html");
         PrintWriter out = resp.getWriter();
-        out.print("<form action='display-prod'>");
-        out.print("Enter Product Name : <input type='text' name='pname'>");
-        out.print("<br/><br/>");
-        out.print("Enter Product Quantity : <input type='text' name='quantity'>");
-        out.print("<br/><br/>");
-        out.print("Enter Product Price : <input type='text' name='price'>");
-        out.print("<button type='submit'>Send</button>");
+        out.print("<form method='POST' action='display-prod'>");
+            out.print("Enter Product Name : <input type='text' name='pname'>");
+            out.print("<br/><br/>");
+            out.print("Enter Product Quantity : <input type='number' name='quantity'>");
+            out.print("<br/><br/>");
+            out.print("Enter Product Price : <input type='text' name='price'>");
+            out.print("<br/><br/>");
+            out.print("<button type='submit'>Send</button>");
         out.print("</form>");
     }
 }
---
+++ code/AdvanceJava/ServletCommunication/src/main/java/ServletLifecycle.java
@@ -0,0 +1,38 @@
+import jakarta.servlet.ServletConfig;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet("/lifecycle")
+public class ServletLifecycle extends HttpServlet {
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        System.out.println("Init Servlet");
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    }
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    }
+    @Override
+    protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    }
+    @Override
+    protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException
+    }
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("Service Servlet");
+    }
+    @Override
+    public void destroy() {
+        System.out.println("Destroy Servlet");
+    }
+}
