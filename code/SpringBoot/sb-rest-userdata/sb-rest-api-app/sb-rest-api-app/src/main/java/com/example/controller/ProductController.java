db41e11509a634c6face99e3f98b8541a86b26e1
snjava <scnaikwadi2@gmail.com>
2026-01-11 13:02:00 +0530

    Spring Boot REST API
    Spring Boot REST API

--- code/SpringBoot/sb-rest-userdata/sb-rest-api-app/sb-rest-api-app/src/main/java/com/example/controller/ProductController.java
+++ code/SpringBoot/sb-rest-userdata/sb-rest-api-app/sb-rest-api-app/src/main/java/com/example/controller/ProductController.java
@@ -2,10 +2,10 @@ package com.example.controller;
 import com.example.dto.Product;
 import com.example.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
 import java.util.List;
 @RestController
 public class ProductController {
@@ -22,4 +22,45 @@ public class ProductController {
     public List<Product> getProducts() {
         return productService.getAllProduct();
     }
+    // http://localhost:8080/swagger-ui/index.html
+    //http://localhost:8080/delete-by-id?pid=2
+    @DeleteMapping("/delete-by-id")
+    public String deleteById(@RequestParam("pid") int id) {
+        boolean result = productService.deleteProduct(id);
+        return result
+                ? "Product DELETED Successfully"
+                : "NO Product found for id : " + id;
+    }
+    //http://localhost:8080/update-product-details
+    @PutMapping("/update-product-details")
+    public String updateProduct(@RequestBody Product product) {
+        boolean result = productService.updateProduct(product);
+        return result
+                ? "Product UPDATED Successfully"
+                : "NO Product found";
+    }
+    //http://localhost:8080/update-product-price/1/23242
+    @PatchMapping("/update-product-quantity/{pid}/{pqty}")
+    public String updateProductQuantity(@PathVariable(name = "pid") int id,
+                                        @PathVariable(name = "pqty") int quantity) {
+        boolean result = productService.updateProductQuantity(id, quantity);
+        return result
+                ? "Product Quantity UPDATED Successfully"
+                : "NO Product found for ID : " + id;
+    }
+    @GetMapping("/get-product-by-id/{pid}")
+    public ResponseEntity getProductById(@PathVariable(name = "pid") int id) {
+        Product product = productService.getProductById(id);
+        if (product != null) {
+            return ResponseEntity.status(HttpStatus.OK).body(product);
+        } else {
+            return ResponseEntity.status(HttpStatus.NO_CONTENT).body("No Product");
+        }
+    }
 }
+
+
+
+
+
+
--- code/SpringBoot/sb-rest-userdata/sb-rest-api-app/sb-rest-api-app/src/main/java/com/example/service/ProductService.java
+++ code/SpringBoot/sb-rest-userdata/sb-rest-api-app/sb-rest-api-app/src/main/java/com/example/service/ProductService.java
@@ -13,4 +13,41 @@ public class ProductService {
     public List<Product> getAllProduct() {
         return productList;
     }
+    public boolean deleteProduct(int id) {
+        Product product = getProductById(id);
+        return productList.remove(product);
+    }
+    public boolean updateProduct(Product product) {
+        Product oldProduct = getProductById(product.getId());
+        if(oldProduct==null) {
+            return false;
+        }
+        oldProduct.setName(product.getName());
+        oldProduct.setPrice(product.getPrice());
+        oldProduct.setQuantity(product.getQuantity());
+        oldProduct.setReviews(product.getReviews());
+        return true;
+    }
+    public boolean updateProductQuantity(int id, int quantity) {
+        Product product = getProductById(id);
+        if(product == null) {
+            return false;
+        }
+        product.setQuantity(quantity);
+        return true;
+    }
+    public Product getProductById(int id) {
+        Product pd = null;
+        for(Product product : productList) {
+            if(product.getId() == id) {
+                pd = product;
+                break;
+            }
+        }
+        return pd;
+        /*return productList.stream()
+                .filter(product -> product.getId() == id)
+                .findFirst()
+                .get();*/
+    }
 }
